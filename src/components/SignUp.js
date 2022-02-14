b2f890a297d325b4e73ad7bde83d8af695fdffc6
JoshGenereux <josh.genereux13@gmail.com>
2022-02-14 12:03:00 -0700

    added some styling

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,35 +1,81 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate} from "react-router";
 
 const SignUp = ()=>{
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirm, setconfirm] = useState('')
+  const [user, setUser] = useState(null)
+
+  let nav = useNavigate();
+
+  const handleSubmit = (e)=>{
+    const home = '/home';
+    e.preventDefault();
+    if(confirm !== password){
+      alert('Passwords must match.')
+    } else {
+      setUser({
+        firstName,
+        lastName,
+        email,
+        username,
+        password,
+        confirm
+      })
+      nav(home);
+    }
+  }
+
+  const handleFirstname = (e)=> setFirstName(e.target.value);
+  const handleLastname = (e)=> setLastName(e.target.value);
+  const handleEmail = (e)=> setEmail(e.target.value);
+  const handleUsername = (e)=> setUsername(e.target.value);
+  const handlePassword = (e)=> setPassword(e.target.value);
+  const handleConfirm = (e)=> {setconfirm(e.target.value)}
 
   return (
     <div className='signup-container'>
-      <form className='signup-form'>
+      <form className='signup-form' onSubmit={handleSubmit}>
         <input
+          onChange={handleFirstname}
           className='signup-input'
           type='text'
           placeholder='first name'/>
         <input
+          onChange={handleLastname}
           className='signup-input'
           type='text'
           placeholder='last name'/>
         <input
+          onChange={handleEmail}
           className='signup-input'
           type='email'
           placeholder='email'/>
         <input
+          onChange={handleUsername}
           type='text'
           className='signup-input'
           placeholder='username'/>
         <input
+          onChange={handlePassword}
           className='signup-input'
           type='password'
           placeholder='password'/>
         <input
+          onChange={handleConfirm}
           className='signup-input'
           type='password'
           placeholder='confirm password'/>
-        <button className='login-submit-btn'>Sign Up</button>
+        <button
+          className='login-submit-btn'
+          type='submit'
+          onClick={()=>console.log(user)}
+        >Sign Up
+        </button>
       </form>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+
+      </Routes>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
